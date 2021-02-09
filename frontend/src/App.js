c73d5df764d3d5116442c8b947c0efdfb2b469a8
ryanklintworth <ryan.klintworth@gmail.com>
2021-02-08 20:05:02 -0600

    post.js and styles.js files initiallized

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,15 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import yearbook from './images/yearbook-logo.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
+import useStyles from './styles';
 
 const App = () => {
+  const classes = useStyles();
   return (
     <Container maxwidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Sign My Yearbook</Typography>
-        <img src={yearbook} alt="yearbook" />
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center"></Typography>
+        <img className={classes.image} src={yearbook} alt="yearbook" height="300" />
       </AppBar>
       <Grow in>
         <Container>
