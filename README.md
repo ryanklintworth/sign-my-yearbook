65d593061c7b38ee74beae5e8f52b0935d5e0b3a
ryanklintworth <ryan.klintworth@gmail.com>
2021-02-09 13:48:25 -0600

    hooks created for the api

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Sign My Yearbook
+# <div align="center">Sign My Yearbook</div>
 
-<img src="GA-YB.png">
+ <img src="GA-YB.png" />
 
 ### I've created an application for my fellow software engineering graduates to digital "sign my yearbook"
  
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts';
 import yearbook from './images/GA-YB.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
@@ -7,6 +9,12 @@ import useStyles from './styles';
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts);
+  }, [dispatch]);
+
   return (
     <Container maxwidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
