ccb1f0dedb7d09ecb6b1d97f877d5fd7b25eb291
ryanklintworth <ryan.klintworth@gmail.com>
2021-02-08 19:41:09 -0600

    App.js form and grid setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import yearbook from './images/yearbook-logo.png';
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Sign My Yearbook</Typography>
+        <img src={yearbook} alt="yearbook" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   )
 }
 
-export default App;
+export default App; 
