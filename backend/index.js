889aa858dfc796c08b7d4384db121648bfcc495f
ryanklintworth <ryan.klintworth@gmail.com>
2021-02-08 18:10:00 -0600

    index.js server is succesfully running

--- backend/index.js
+++ backend/index.js
@@ -3,3 +3,17 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://ryanklintworth:TWybAXFNTUnyOOBp@cluster0.k582b.mongodb.net/<dbname>?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false)
