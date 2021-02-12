a81b69338d72df5891d125e3fddd429df428f162
ryanklintworth <ryan.klintworth@gmail.com>
2021-02-12 16:54:52 -0600

    frontend finished

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes)
 
+app.get('/', (req,res) => {
+  res.send('Yearbook API')
+})
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
